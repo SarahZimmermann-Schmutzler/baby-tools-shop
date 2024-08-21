3ae2df2f928a5ea0b832309ba01d2cae3e4e2144
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-08-21 09:56:13 +0200

    script supe for creating automatically superuser when start container

---
+++ babyshop_app/management/commands/createsupe.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+import dotenv
+import os
+
+class Command(BaseCommand):
+    help = 'Create a superuser non-interactively'
+
+    def handle(self, *args, **options):
+        # .env-Datei laden
+        dotenv.load_dotenv()
+
+        # Werte aus der .env-Datei laden
+        username = os.environ.get('SUPERUSER_USERNAME')
+        email = os.environ.get('SUPERUSER_EMAIL')
+        password = os.environ.get('SUPERUSER_PASSWORD')
+
+        if not username or not email or not password:
+            self.stdout.write(self.style.ERROR('Superuser credentials are missing in the .env file.'))
+            return
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" created successfully!'))
+        else:
+            self.stdout.write(self.style.WARNING(f'Superuser "{username}" already exists.'))
+
