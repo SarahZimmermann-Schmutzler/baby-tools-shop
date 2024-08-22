b29e055c03a10afe5a246fa3154444be392343f9
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-08-22 11:11:58 +0200

    path to templates in settings

--- babyshop_app/babyshop/settings.py
+++ babyshop_app/babyshop/settings.py
@@ -62,7 +62,8 @@ ROOT_URLCONF = 'babyshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        # 'DIRS': ["templates"],
+        'DIRS': [os.path.join(BASE_DIR, 'babyshop_app', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
